caf7f270ed39af180f3670865b93d0a0635cde84
Joseph Sesti <sestijoey63@gmail.com>
2019-09-10 19:41:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,14 @@ class Song
   end
   
   def self.new_from_filename
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].delete(".mp3")
     
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
   
   def self.create_from_filename(filename)
     parts = filename.split(" - ")
